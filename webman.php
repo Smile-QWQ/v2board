672155fb8f24d649c1ff5e80f0404338e43b7999
Smile <smileqwq233@gmail.com>
2024-12-20 15:12:16 +0800

    Docker

--- webman.php
+++ webman.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Adapterman\Adapterman;
 use Workerman\Worker;
 use Illuminate\Support\Facades\Cache;
+use Workerman\Events\EventInterface;
 
 putenv('APP_RUNNING_IN_CONSOLE=false');
 define('MAX_REQUEST', 6600);
@@ -36,5 +37,63 @@ $http_worker->onMessage = static function ($connection, $request) {
         Worker::stopAll();
     }
 };
+
+// 添加 inotify 文件监控功能
+if (extension_loaded('inotify')) {
+    $file_monitor_worker = new Worker();
+    $file_monitor_worker->name = 'FileMonitor';
+    $file_monitor_worker->reloadable = false;
+    $monitor_dirs = ['app', 'bootstrap', 'config', 'resources', 'routes', 'public', '.env'];
+    $monitor_files = [];
+
+    $file_monitor_worker->onWorkerStart = function ($worker) use ($monitor_dirs, &$monitor_files) {
+        if (!extension_loaded('inotify')) {
+            echo "FileMonitor: Please install inotify extension.\n";
+            return;
+        }
+        $worker->inotifyFd = inotify_init();
+        stream_set_blocking($worker->inotifyFd, 0);
+
+        foreach ($monitor_dirs as $monitor_dir) {
+            $monitor_realpath = realpath(__DIR__ . "/{$monitor_dir}");
+            addInotify($monitor_realpath, $worker->inotifyFd, $monitor_files);
+            if (is_file($monitor_realpath)) {
+                continue;
+            }
+            $iterator = new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($monitor_realpath, \FilesystemIterator::SKIP_DOTS),
+                \RecursiveIteratorIterator::SELF_FIRST
+            );
+            foreach ($iterator as $file) {
+                if ($file->isDir()) {
+                    $realpath = realpath($file);
+                    addInotify($realpath, $worker->inotifyFd, $monitor_files);
+                }
+            }
+        }
+
+        Worker::$globalEvent->add($worker->inotifyFd, EventInterface::EV_READ, function () use ($worker, &$monitor_files) {
+            checkFilesChange($worker->inotifyFd, $monitor_files);
+        });
+    };
+}
+
+function addInotify(string $realpath, $fd, &$monitor_files)
+{
+    $wd = inotify_add_watch($fd, $realpath, IN_MODIFY | IN_CREATE | IN_DELETE);
+    $monitor_files[$wd] = $realpath;
+}
+
+function checkFilesChange($inotify_fd, &$monitor_files)
+{
+    $events = inotify_read($inotify_fd);
+    if ($events) {
+        foreach ($events as $ev) {
+            $file = $monitor_files[$ev['wd']];
+            echo $file . "/{$ev['name']} updated. Reloading...\n";
+        }
+        posix_kill(posix_getppid(), SIGUSR1);
+    }
+}
 
 Worker::runAll();
